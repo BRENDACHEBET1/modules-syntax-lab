0caf945f8d4d037676950139d907a3e9845e325a
BRENDACHEBET1 <chebetb388@gmail.com>
2026-03-28 22:42:33 +0300

    Initial Commit

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-
+require('datejs')
+function combineUsers(...args){
+  const combinedObject = {
+    users: [],
+    merge_date: ""
+  }
+  //Using loop to isolate each array then using spread to merge them 
+  for(const array of args){
+    combinedObject.users.push(...array)
+  }
+  // Adding an attribute to combined Object
+  combinedObject.merge_date = Date.today().toString('M/d/yyyy')
+ 
+  return combinedObject
+}
 
 
 module.exports = {
